ae4f9cf62ff5272a25382c5d1cac682c1f8e774c
phamthinh14 <thinhpham0717@gmail.com>
2022-05-07 21:23:17 -0400

    Added an add-component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ExpenseListComponent } from './components/expense-list/expense-list.component';
+import { AddExpenseComponent } from './components/add-expense/add-expense.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExpenseListComponent
+    ExpenseListComponent,
+    AddExpenseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
 
   ],
   providers: [],
