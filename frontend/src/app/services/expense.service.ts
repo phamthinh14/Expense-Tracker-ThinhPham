6d1926fe936e0618794902ccd568d5044f50710d
phamthinh14 <thinhpham0717@gmail.com>
2022-05-07 20:54:06 -0400

    Added components, models, services to retrive data from the back end

---
+++ frontend/src/app/services/expense.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Expense } from '../models/expense';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpenseService {
+
+  private baseUrl: string = 'http://localhost:8080/api/v1/expenses';
+  constructor(private httpClient: HttpClient) { }
+
+  getExpenses(): Observable<Expense[]> {
+    return this.httpClient.get<Expense[]>(this.baseUrl);
+  }
+}
