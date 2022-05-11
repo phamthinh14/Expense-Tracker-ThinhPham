25e48f1642568b1aa7313d84d8f1de0bd4a8c1e9
phamthinh14 <thinhpham0717@gmail.com>
2022-05-11 17:02:55 -0400

    Added a default sort according to 'name' and filter feature in front end Added tests for service layer and controller layer

--- frontend/src/app/components/expense-list/expense-list.component.ts
+++ frontend/src/app/components/expense-list/expense-list.component.ts
@@ -26,7 +26,22 @@ export class ExpenseListComponent implements OnInit {
 
   listAllExpensesHelper() {
     this.expenseService.getExpenses().subscribe(
-      data => this.expenses = data
+      data => this.expenses = this.filterAndSortExpenese(data)
+    );
+  }
+
+  filters = {
+    keyword: '',
+    sortBy: 'Name'
+  }
+
+  filterAndSortExpenese(expenses: Expense[]) {
+    return expenses.filter(e => {
+      return e.productName.toLowerCase().includes(this.filters.keyword.toLowerCase());
+    }).sort(
+      (a, b) => {
+        return a.productName.toLowerCase() < b.productName.toLowerCase() ? -1 : 1;
+      }
     );
   }
 
