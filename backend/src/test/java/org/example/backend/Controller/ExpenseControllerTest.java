9ad03e726d7775a56bede3f62b4e1c91a25018be
phamthinh14 <thinhpham0717@gmail.com>
2022-05-11 23:53:26 -0400

    Added a new display for the display list in the front end Added new tests in the Controller layer

--- backend/src/test/java/org/example/backend/Controller/ExpenseControllerTest.java
+++ backend/src/test/java/org/example/backend/Controller/ExpenseControllerTest.java
@@ -1,6 +1,8 @@
 package org.example.backend.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.example.backend.Model.Expense;
 import org.example.backend.Service.ExpenseService;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,13 +14,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.math.BigDecimal;
@@ -28,18 +27,17 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 class ExpenseControllerTest {
     private MockMvc mockMvc;
-    @Autowired
-    ObjectMapper mapper;
     @Mock
     private ExpenseService expenseService;
     @InjectMocks
     private ExpenseController expenseController;
-
+    private ObjectWriter objectWriter = new ObjectMapper().writer();
     Expense givenExpense1;
     Expense givenExpense2;
     Expense givenExpense3;
@@ -67,6 +65,23 @@ class ExpenseControllerTest {
 
     @Test
     void saveProduct() {
+        Expense givenExpense4 = new Expense(4L, "Muffin", "Sweet Tooth", new BigDecimal(5.99));
+
+        try {
+            String content = objectWriter.writeValueAsString(givenExpense4);
+            System.out.println(content);
+            MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/api/v1/expenses")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .content(content);
+            mockMvc.perform(mockRequest)
+                    .andExpect(status().isOk());
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Test
@@ -79,7 +94,7 @@ class ExpenseControllerTest {
                             .get("/api/v1/expenses")
                             .contentType(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(3)));
+                    .andExpect(jsonPath("$", hasSize(3)));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -87,26 +102,47 @@ class ExpenseControllerTest {
 
     @Test
     @DisplayName("Detail of fetch all")
-    void fetchDetailExpensesTests(){
+    void fetchDetailExpensesTests() {
         List<Expense> expenseList = new ArrayList<>(Arrays.asList(givenExpense1, givenExpense2, givenExpense3));
         Mockito.when(this.expenseService.fetchAll()).thenReturn(expenseList);
         try {
             mockMvc.perform(MockMvcRequestBuilders
                             .get("/api/v1/expenses")
                             .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(MockMvcResultMatchers.jsonPath("$[1].description", is("An old OS")))
-                    .andExpect( MockMvcResultMatchers.jsonPath("$[0].cost", is(new BigDecimal(95.99))))
-                    .andExpect(MockMvcResultMatchers.jsonPath("$[2].productName", is("Windows")));
+                    .andExpect(jsonPath("$[1].description", is("An old OS")))
+                    .andExpect(jsonPath("$[0].cost", is(new BigDecimal(95.99))))
+                    .andExpect(jsonPath("$[2].productName", is("Windows")));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     @Test
+    @DisplayName("Fetch by ID")
     void fetchExpenseById() {
+        Mockito.when(this.expenseService.fetchById(3L)).thenReturn(givenExpense3);
+        try {
+            mockMvc.perform(MockMvcRequestBuilders
+                            .get("/api/v1/expenses/3")
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.productName", is("Windows")))
+                    .andExpect(jsonPath("$.cost", is(new BigDecimal(195.59))));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     void deleteExpense() {
+
+        try {
+            mockMvc.perform(MockMvcRequestBuilders
+                    .delete("/api/v1/expenses/2")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
